21336568dd69d98ed6c7f8e28612c0a9cdd0ef40
Александр <scshepin@yandex.ru>
2021-10-23 22:10:51 +0800

    update few files

--- app/src/main/java/Alexnds/myhomework/MainActivity.kt
+++ app/src/main/java/Alexnds/myhomework/MainActivity.kt
@@ -1,11 +1,48 @@
 package Alexnds.myhomework
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}
+
+    fun ClickMyBtn(view: android.view.View) {
+        setContentView(R.layout.myscreen)
+    }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+    override fun finishActivity(requestCode: Int) {
+        super.finishActivity(requestCode)
+    }}
+
+
+
+
+
+
